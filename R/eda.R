003ed7740f0df5627c45e08528334fff1d1c3e25
Patrick S. Eastham <patrick.eastham@certara.com>
2022-04-22 10:26:58 -0400

    initial commit

---
+++ R/eda.R
@@ -0,0 +1,7 @@
+#' @export
+testfunc <- function() {
+  text <- paste("This is a test function in the pmx6 library.",
+                "It was created on April 22, 2022.",
+                sep="\n")
+  print(text)
+}
