98761e78a11f31e96796bb36d65c5d7fe556ed51
Patrick S. Eastham <patrick.eastham@certara.com>
2022-06-17 06:52:01 -0400

    Added some functions

--- R/eda.R
+++ R/eda.R
@@ -5,3 +5,75 @@ testfunc <- function() {
                 sep="\n")
   print(text)
 }
+
+#' modify plots to log scale (helper function - do not export)
+add_log_modifications <- function(p) {
+  p <- p + ggplot2::scale_y_log10()
+  return(p)
+}
+
+#' Creates a mean plot object with LOESS curve representing mean
+#'
+#' @param df input data frame
+#' @param time column variable representing time, typically "TIME"
+#' @param dv column variable representing dependent variable, typically "DV"
+#' @param scale either "linear" or "semilog"
+#' @param xval string for name of x value
+#' @param xunit string for unit of x value
+#' @param yval string for name of y value
+#' @param yunit string for unit of y value
+#' @param facet_on facet variable, e.g. "RACE", "SEX", etc
+#' @export
+mean_plot <- function(df,
+                      time="TIME",
+                      dv="DV",
+                      scale='linear',
+                      xval="Actual time since first dose",
+                      xunit="hr",
+                      yval="Drug concentration",
+                      yunit="ng/mL",
+                      facet_on="NOFACET") {
+  dat <- df
+
+  if (facet_on != "NOFACET") { dat %<>% mutate(FACETVAR = !!sym(facet_on)) }
+
+  p <- ggplot2::ggplot(dat,
+                       aes(x=!!sym(time),
+                           y=!!sym(dv))) +
+                       geom_point(alpha=0.8, size = 2) +
+                       geom_smooth(span = 0.5) +
+                       labs(x = paste0(xval, "(", xunit, ")"),
+                            y = paste0(yval, "(", yunit, ")")) +
+                       theme_bw() +
+                       expand_limits(y=0) +
+                       guides(color = guide_legend(override.aes = list(size = 3, alpha = 1)))
+
+  if (facet_on != "NOFACET") { p <- p + ggplot2::facet_wrap(~FACETVAR) }
+  if (scale == 'semilog') { p %<>% add_log_modifications() }
+
+  return(p)
+}
+
+# function for correlation matrix - from Prokash's EDA script
+lowerFn <- function(data, mapping, method = "lm", ...) {
+  p <- ggplot2::ggplot(data = data, mapping = mapping) +
+       ggplot2::geom_point(colour = "blue") +
+       ggplot2::geom_smooth(method = method, color = "red", ...)
+  return(p)
+}
+
+#' Creates a correlation matrix with covariates
+#'
+#' @param df input data frame
+#' @examples
+#' dat <- df %>% dplyr::distinct(USUBJID, .keep_all=TRUE) %>% dplyr::select(AGE, BWT, BHT, BBMI, SEXF)
+#' p <- correlation.matrix(dat)
+#' print(p)
+#' @export
+correlation.matrix <- function(df) {
+  p <- GGally::ggpairs(dat,
+                       lower = list(continuous = wrap(lowerFn, method = "lm")),
+                       diag = list(continuous = wrap("barDiag", colour = "blue")),
+                       upper = list(continuous = wrap("cor", size = 4)))
+  return(p)
+}
--- R/util.R
+++ R/util.R
@@ -1,50 +1,52 @@
 #' Creates a new Rmarkdown html tab.
-#' 
+#'
 #' Make sure that Rmd chunk option results='asis' is present.
-#' 
-#' @param item An object to add into a new Rmd tab
+#'
 #' @param title Tab title
 #' @param header An int indicating Rmd html header level (e.g. 3 means "###")
 #' @param numbered A boolean for whether tab should be numbered or not
 #' @param subtabs A boolean for whether tab has subtabs below it or not
 #' @param pills A boolean for whether tab should be pill-shaped or not
-#' @param table A boolean for whether `item` is a table object or not
 #'
 #' @examples
 #' p <- ggplot(x, y, dat=somedata)
-#' new_tab(p, title="My Plot", header=3)
+#' new_tab(title="My Plot", header=3)
+#' print(p)
 #' @export
-new_tab <- function(item, title="Tab", header=2, numbered=F, subtabs=F, pills=F, table=F) {
+new_tab <- function(title="Tab", header=2, nonumber=T, subtabs=F, pills=F) {
+  cat("  \n\n")
+
   # create header hashtags
   HEAD = ""
-  for (i in 1:header) { 
+  for (i in 1:header) {
     HEAD = paste0(HEAD, "#")
   }
-  
+
   # check if there are any options
-  ANYOPTIONS = subtabs | !numbered
-  
+  ANYOPTIONS = subtabs | nonumber
+
   # create subtabs and numbering indicators
   OPTIONS = ""
   if (ANYOPTIONS) {
     OPTIONS = paste0(OPTIONS, "{")
-    if (!numbered) { OPTIONS = paste0(OPTIONS, "- ") }
+    if (nonumber) { OPTIONS = paste0(OPTIONS, "- ") }
     if (subtabs){
       OPTIONS = paste0(OPTIONS, ".tabset .tabset-fade")
       if (pills) { OPTIONS = paste0(OPTIONS, " .tabset-pills") }
     }
     OPTIONS = paste0(OPTIONS, "}")
   }
-  
-  # print item to html in new tab
-  cat("  \n")
+
   cat(HEAD, " ", title, " ", OPTIONS, "  \n\n")
-  if ( is.null(item) ) {
-    item
-  } else if ( table ){
-    return(item)
-  } else {
-    print(item)
-  }
+
   cat("  \n\n")
 }
+
+
+# Add function for splitting up continuous variable into quartiles cleanly
+
+
+# Add function for adding number of subjects with that facet variable to title (e.g. "Male (n=23)" instead of just "Male")
+
+
+# Add function that inserts "CERTARA" logo into html
