cc056373607b0d3443d5dd69777ba083e8398b12
Boris Grinshpun <boris.grinshpun@certara.com>
2022-07-14 13:31:13 -0400

    eda test update

--- R/eda.R
+++ R/eda.R
@@ -1,5 +1,5 @@
-#' @export
 testfunc <- function() {
+  #' @export
   text <- paste("This is a test function in the pmx6 library.",
                 "It was created on April 22, 2022.",
                 sep="\n")
